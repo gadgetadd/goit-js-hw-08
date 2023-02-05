004a3223d7e9275802d52e9ec26c61d2ad7360d6
Maksym Ostapchuk <gadget.add@gmail.com>
2023-02-05 18:25:54 +0200

    task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,12 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'feedback-form-state';
+const form = document.querySelector('.feedback-form');
+const formData = {};
+
+const onFormInput = event => {
+  formData[event.target.name] = event.target.value;
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+form.addEventListener('input', onFormInput);
