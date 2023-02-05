96e849fded53f9c41702ae7aee240fa2ad844d97
Maksym Ostapchuk <gadget.add@gmail.com>
2023-02-05 23:33:30 +0200

    task3 update

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -9,4 +9,31 @@ const onFormInput = event => {
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 };
 
-form.addEventListener('input', onFormInput);
+const restoreForm = () => {
+  let storageData = localStorage.getItem(STORAGE_KEY);
+  if (storageData) {
+    storageData = JSON.parse(storageData);
+    form.elements.email.value = storageData.email ? storageData.email : '';
+    form.elements.message.value = storageData.message
+      ? storageData.message
+      : '';
+  }
+};
+
+const onFormSubmit = event => {
+  event.preventDefault();
+
+  const submitData = {};
+  submitData.email = event.target.elements.email.value;
+  submitData.message = event.target.elements.message.value;
+  console.log(submitData);
+
+  event.target.reset();
+  formData.email = '';
+  formData.message = '';
+  localStorage.removeItem(STORAGE_KEY);
+};
+
+restoreForm();
+form.addEventListener('input', throttle(onFormInput, 500));
+form.addEventListener('submit', onFormSubmit);
